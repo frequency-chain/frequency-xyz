fa8dc7356767395283575b2bbf21b0fb8ef37e0d
Wil Wade <wil.wade@amplica.io>
2024-02-28 21:39:02 -0500

    Fix slight border issue

--- src/components/Sections/Section2Wave.svelte
+++ src/components/Sections/Section2Wave.svelte
@@ -1,4 +1,6 @@
-<div class={`absolute left-0 h-full w-full bg-left-bottom bg-no-repeat lg:bg-[url(/src/lib/assets/section2waves.svg)]`}>
+<div
+  class={`absolute -bottom-1 left-0 h-full w-full bg-left-bottom bg-no-repeat lg:bg-[url(/src/lib/assets/section2waves.svg)]`}
+>
   <div
     class="h-full w-full bg-left-bottom bg-no-repeat sm:w-[170%] sm:max-w-[900px] sm:bg-[url(/src/lib/assets/section2mobile-top.svg)] md:w-[110%] md:max-w-full lg:hidden"
   />
