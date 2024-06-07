ec8690378f49b7e3f34b32c080d59fe0b3327142
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2024-06-07 15:48:54 -0700

    add error page & redirect (#16)
    # Description
    
    - added error page
    - if 404, redirect home.
    
    - Closes #15 
    
    ## Screenshot(s)
    
    
    https://github.com/frequency-chain/frequency-xyz/assets/43625033/f7f36dd4-bbd9-45a6-8734-a2eae8e59eef
    
    
    
    ## Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    
    # How to Test?
    
    Please describe how to test that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also
    list any relevant details for your test configuration
    
    1. Pull and run locally
    2. Go to path that does not exist
    3. See that user is redirected home
    4. See that on paths the do exist, everything works as expected
    
    # Checklist:
    
    - [x] I have performed a self-review of my code
    - [x] I have commented my code & PR, particularly in hard-to-understand
    areas
    - [x] I have checked at all the breakpoints to make sure it works on all
    screen sizes
    
    ---------
    
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from '$app/stores';
+
+  $: {
+    if ($page.status === 404) {
+      window.location.href = '/';
+    }
+  }
+</script>
+
+<div class="freq-container mt-[150px]">
+  <p>Error</p>
+  <a class="underline" href="/">Return Home</a>
+</div>
