3fb46550997d411bb89c46e531cda1bdb30e4b52
Wil Wade <wil.wade@amplica.io>
2024-09-13 20:14:21 +0000

    About goes to the correct place on other pages

--- src/components/Header/NavItems.svelte
+++ src/components/Header/NavItems.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   export let textColor: string;
   export let section: number;
   export let isDesktop: boolean = true;
+
+  let isOnHomePage = $page.url.pathname === '/';
 </script>
 
-{#if isDesktop}
+{#if !isOnHomePage}
+  <a class={`underline-on-hover after:bg-${textColor}`} href="/">About.</a>
+{:else if isDesktop}
   <a class={`underline-on-hover after:bg-${textColor}`} href="/" on:click|preventDefault={() => (section = 1)}>About.</a
   >
 {/if}
