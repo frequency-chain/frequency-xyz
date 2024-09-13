b1564c2e239380a24bacf10ffd8c4c5ae440b309
Wil Wade <wil.wade@amplica.io>
2024-09-13 19:55:30 +0000

    Add the Privacy Policy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,3 @@
-<script>
-  import Footer from '../components/Footer.svelte';
-  import JoinSocials from '../components/JoinSocials.svelte';
-  import GetNotified from '../components/GetNotified/GetNotified.svelte';
-  import SideTag from '../components/SideTag.svelte';
-</script>
-
-<div class="max-w-page overflow-x-hidden shadow-xl">
-  <div class="h-[100vh] w-[100vw] overflow-x-hidden">
-    <div class="min-h-[calc(100vh-100px)]">
-      <slot />
-      <div class="freq-container mt-[150px]">
-        <JoinSocials />
-      </div>
-    </div>
-    <Footer />
-    <SideTag triggerText="Get notified">
-      <GetNotified />
-    </SideTag>
-  </div>
+<div class="max-w-page overflow-x-hidden shadow-xl print:shadow-none">
+  <slot />
 </div>
