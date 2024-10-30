bc56c764c9a6fa42e1d6ea59d3acd850b68951e9
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-10-30 11:17:54 -0500

    cleanup

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -3,22 +3,8 @@
   import SectionDocs from '../../components/Sections/SectionDocs.svelte';
   import SectionNavigation from '../../components/Sections/SectionNavigation.svelte';
   import Sections from '../../components/Sections/Sections.svelte';
-  import Header from '../../components/Header/Header.svelte';
 
   $: section = 1;
-
-  const brightBlue = '#5E69FF';
-  const cream = '#FEFAF3';
-
-  const logoMap = new Map([
-    [1, { logo: brightBlue, nav: 'navy' }],
-    [2, { logo: cream, nav: 'cream' }],
-    [3, { logo: brightBlue, nav: 'cream' }],
-    [4, { logo: brightBlue, nav: 'navy' }],
-    [5, { logo: cream, nav: 'cream' }],
-  ]);
-
-  // Section navigation titles for Aria
   const sectionTitles = ['Introduction'];
 </script>
 
