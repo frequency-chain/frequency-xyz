ee78267a2f479c27e1bc90ccb744825556a90be5
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-10-31 10:59:37 -0500

    remove image from mobile view

--- src/routes/docs/components/Placeholder.svelte
+++ src/routes/docs/components/Placeholder.svelte
@@ -1,8 +1,3 @@
-<!-- Mobile -->
-<div
-  class={`absolute bottom-0 w-full bg-[url(/src/lib/assets/placeholder-portal.svg)] bg-no-repeat sm:h-[180px] sm:bg-right-bottom md:h-[270px] lg:hidden vertical-lg:relative vertical-lg:block`}
-></div>
-
 <!-- Desktop -->
 <div
   class="relative z-10 h-full w-full bg-[url(/src/lib/assets/placeholder-portal.svg)] bg-right-bottom bg-no-repeat sm:hidden lg:block vertical-lg:hidden"
