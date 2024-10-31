90aa32ec09bf7114451a4eebda51c85c74668a2f
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-10-31 15:11:04 -0500

    set page layout

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+  import Header from '../../components/Header/Header.svelte';
   import { Footer } from '@frequency-chain/style-guide';
+
+  const logoHex = '#5E69FF';
+  const navColor = 'navy';
 </script>
 
-<div>
-  <slot />
+<div class="relative lg:max-w-[100vw] xl:max-w-page">
+  <Header {logoHex} {navColor} />
+  <div class="mt-4 p-3">
+    <slot />
+  </div>
   <Footer />
 </div>
--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,26 +1,7 @@
 <script lang="ts">
   import DocsBody from './components/DocsBody.svelte';
-  import Header from '../../components/Header/Header.svelte';
-  $: section = 1;
-  const brightBlue = '#5E69FF';
-  const cream = '#FEFAF3';
-
-  const logoMap = new Map([
-    [1, { logo: brightBlue, nav: 'navy' }],
-    [2, { logo: cream, nav: 'cream' }],
-    [3, { logo: brightBlue, nav: 'cream' }],
-    [4, { logo: brightBlue, nav: 'navy' }],
-    [5, { logo: cream, nav: 'cream' }],
-  ]);
-
-  // Section navigation titles for Aria
-
-  $: logoHex = logoMap.get(section)?.logo;
-  $: navColor = logoMap.get(section)?.nav;
-  // $: baseUrl = $page.url.protocol + '//' + $page.url.host;
 </script>
 
-<div class="absolute top-0 z-50 w-full"><Header {logoHex} {navColor} bind:section /></div>
 <div class={`relative w-[100vw] min-w-full sm:pt-[80px] lg:pt-[140px] vertical-lg:w-[100%]`}>
   <DocsBody />
 </div>
