e5fe1a36a59387d55d9d516fcc094aaa7dbf4afa
Joe Caputo <joseph.caputo@projectliberty.io>
2024-11-06 17:04:54 -0500

    feat: explore section first pass

---
+++ src/components/HAccordion.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { Button } from '@frequency-chain/style-guide';
+  import Vector from '../lib/assets/Vector.svg';
+
+  const dispatch = createEventDispatcher();
+
+  let {
+    state = $bindable('closed'),
+    sectionNumber,
+    sectionLabel,
+    iconSrc,
+  }: { state: 'open' | 'closed'; sectionNumber: number | string; sectionLabel: string; iconSrc: string } = $props();
+  //   export let state: 'open' | 'closed' = $state('open');
+  //   export let sectionNumber: string;
+  //   export let sectionLabel: string = 'Section<br/>label';
+  //   export let iconSrc: string | null = null;
+
+  function toggle() {
+    if (state === 'open') {
+      state = 'closed';
+    } else {
+      state = 'open';
+    }
+
+    dispatch('toggled', { index: sectionNumber, state });
+  }
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div
+  id={`explore_${sectionNumber}`}
+  onclick={toggle}
+  class="justify-left border-grayBorder m:w-min-[112px] m:pr-3 flex flex-col border-l-[1.5px] border-opacity-25 pl-3 sm:items-end lg:min-w-[112px] lg:items-start lg:pr-3"
+>
+  <div class="flex text-left sm:items-end lg:items-start">
+    <span class="-mt-[9px] text-clip text-nowrap font-title text-h3 font-normal text-white"
+      >.{sectionNumber.toString().padStart(2, '0')}</span
+    >
+  </div>
+  {#if iconSrc}
+    <div id={`explore_${sectionNumber}_icon`} class="pt-[6px]">
+      <img src={iconSrc} alt="" class="h-[65px] w-[65px]" />
+    </div>
+  {/if}
+  <div id={`explore_${sectionNumber}_label`} class="pt-6">
+    <span class="text-clip text-nowrap font-title text-h6 font-normal text-white">
+      {@html sectionLabel}
+    </span>
+  </div>
+
+  {#if state === 'open'}
+    <div
+      id={`explore_${sectionNumber}_content`}
+      class=" paragraph-spacing-right lg:max-w-auto m:max-w-[280px] m:pb-0 overflow-hidden pt-4 text-left font-sans text-sm text-white sm:max-w-[515px] sm:pb-[100px] sm:pr-5 lg:max-w-[280px] lg:pb-0 lg:pl-0"
+    >
+      <slot>Insert section content here</slot>
+    </div>
+    <div id={`explore_${sectionNumber}_button`} class="m:pt-4 lg:pt-4">
+      <Button size="xs" type="primary"
+        ><span class=" text-xs text-black">Learn</span> <img src={Vector} width="12px" height="12px" alt="" /></Button
+      >
+    </div>
+  {/if}
+</div>
---
+++ src/components/Sections/ExploreSection.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import HAccordion from '../HAccordion.svelte';
+  import ProviderIcon from '../../lib/assets/icon-provider.svg';
+  import GatewayIcon from '../../lib/assets/icon-gateway.svg';
+  import SsoIcon from '../../lib/assets/icon-sso.svg';
+  import CoreIcon from '../../lib/assets/icon-core.svg';
+
+  let sections: Array<'open' | 'closed'> = ['open', 'open', 'closed', 'closed', 'closed'];
+
+  function handleToggled(event: CustomEvent<{ index: string; state: 'open' | 'closed' }>) {
+    const { index, state } = event.detail;
+    const newState = state === 'open' ? 'closed' : 'open';
+    sections.forEach((_state, objIndex, array) => {
+      if (objIndex.toString() !== index) {
+        console.log(`Setting object index ${objIndex} to ${newState}`);
+        array[objIndex] = newState;
+      }
+    });
+  }
+</script>
+
+<div
+  id="explore_section"
+  class="vertical-lg:min-h[550-px] relative flex w-[100vw] min-w-full flex-row bg-navy sm:pb-[40px] sm:pt-[80px] lg:pb-0 lg:pl-[127px] lg:pt-[96px] vertical-lg:w-[100%]"
+>
+  <div id="explore_description" class="relative flex flex-row bg-navy text-teal vertical-lg:min-h-[550px]">
+    <div class="justify-left flex gap-12 sm:flex-col sm:items-end lg:flex-col lg:items-start">
+      <div class="flex text-left sm:items-end lg:items-start">
+        <span class="-mt-[9px] text-clip text-nowrap font-title text-[65px] font-normal sm:max-w-[280px] sm:pr-5"
+          >Explore &amp; Build</span
+        >
+      </div>
+
+      <div
+        class="paragraph-spacing-right lg:max-w-auto text-left font-sans text-sm text-white sm:max-w-[515px] sm:pb-[100px] sm:pr-5 lg:pb-[200px] lg:pl-0"
+      >
+        Frequency offers simple blockchain integration for your applications&mdash;bridging the gap between your app and
+        the decentralized web.  Frequency Gateway offers a suite of tools and services that make it easy to connect your
+        applications to the Frequency blockchain. This allows your development team to focus on building outstanding
+        user experiences without managing the complexity of blockchain interactions. With Gateway you can pick and
+        choose from to build the best applications for your users to:
+        <div class="pt-4 font-bold">
+          <ul class="list-inside list-disc lg:pl-5">
+            <li>Add decentralized authentication and onboarding workflows</li>
+            <li>Connect your users with their universal social graph</li>
+            <li>Read, write, and interact with social media content</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <HAccordion
+    sectionNumber="1"
+    sectionLabel="Quick Start/<br/>Become a Provider"
+    iconSrc={ProviderIcon}
+    on:toggled={handleToggled}
+    bind:state={sections[1]}
+  >
+    In 5 minutes, you can become a provider, set up the Social App Template Example Application that uses Gateway
+    Services. This will give you a quick introduction to a working integration with Gateway Services and a starting
+    place to explore the possibilities.
+  </HAccordion>
+
+  <HAccordion
+    sectionNumber="2"
+    sectionLabel="Frequency<br />Gateway"
+    iconSrc={GatewayIcon}
+    on:toggled={handleToggled}
+    bind:state={sections[2]}
+    >is a collection of easy-to-deploy: microservices that provide simple REST APIs for interacting with Frequency.
+    Configure and deploy only the components you need, including.</HAccordion
+  >
+
+  <HAccordion
+    sectionNumber="3"
+    sectionLabel="Add Single<br />Sign-on to<br />your App"
+    iconSrc={SsoIcon}
+    on:toggled={handleToggled}
+    bind:state={sections[3]}
+    >SIWF provides comprehensive documentation and tools to integrate with Frequency Access, an easy-to-use single
+    sign-on identity solution for users on Frequency. SIWF simplifies the authentication process, allowing users to
+    access the entire Frequency ecosystem effortlessly while maintaining control over their personal data.</HAccordion
+  >
+
+  <HAccordion
+    sectionNumber="4"
+    sectionLabel="Frequency<br />Core"
+    iconSrc={CoreIcon}
+    on:toggled={handleToggled}
+    bind:state={sections[4]}
+    >Here you will find detailed information about the building blocks of Frequency including technical overviews,
+    advanced tutorials, API documentation, code samples and more–everything you need to build your first Frequency
+    application.</HAccordion
+  >
+</div>
