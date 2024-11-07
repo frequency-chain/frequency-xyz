15f7457fdfd41ee797bb01b401e3cde35dab5202
wilwade <wilwade@users.noreply.github.com>
2024-11-07 17:57:31 +0000

    Deploying to gh-pages from @ frequency-chain/frequency-xyz@c6f6722e9a2d5d9b3a1a4e40b24d83b9794dc3ec 🚀

---
+++ _pr/42/_app/immutable/chunks/utils.D8nJk1fh.js
@@ -0,0 +1,1 @@
+import{D as b,x as e}from"./runtime.Cq8ZWjRo.js";function c(n,r,s){if(n==null)return r(void 0),s&&s(void 0),b;const u=e(()=>n.subscribe(r,s));return u.unsubscribe?()=>u.unsubscribe():u}export{c as s};
