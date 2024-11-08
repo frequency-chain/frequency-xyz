479deb9a93465de6a7de32aeec6c5223fec5b9c3
JoeCap08055 <JoeCap08055@users.noreply.github.com>
2024-11-08 21:34:54 +0000

    Deploying to gh-pages from @ frequency-chain/frequency-xyz@f163574599986972388e1370850cc908234c1944 🚀

---
+++ _pr/62/_app/immutable/chunks/utils.BG_AGzOp.js
@@ -0,0 +1,1 @@
+import{n as b,f as e}from"./runtime.Jj8XJZyx.js";function c(n,r,s){if(n==null)return r(void 0),s&&s(void 0),b;const u=e(()=>n.subscribe(r,s));return u.unsubscribe?()=>u.unsubscribe():u}export{c as s};
