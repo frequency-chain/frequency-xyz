5278efda0b2938caf6a863c4374211e27985568a
Wil Wade <wil.wade@projectliberty.io>
2024-11-11 20:11:23 +0000

    Is Active

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import { Header } from '@frequency-chain/style-guide';
   import { base } from '$app/paths';
+  import { page } from '$app/stores';
 
   const menuItems = [
-    { label: 'Mission', href: base + '#mission', viewportHighlightId: 'mission' },
-    { label: 'About', href: base + '#about', viewportHighlightId: 'about' },
-    { label: 'Users', href: base + '#users', viewportHighlightId: 'users' },
+    { label: 'Mission', href: base + '/#mission', viewportHighlightId: 'mission' },
+    { label: 'About', href: base + '/#about', viewportHighlightId: 'about' },
+    { label: 'Users', href: base + '/#users', viewportHighlightId: 'users' },
     { label: 'Developers', href: base + '#developers', viewportHighlightId: 'developers' },
-    { label: 'Contact', href: base + '#contact', viewportHighlightId: 'contact' },
-    { label: 'Developer Portal', href: base + '/docs', isButton: true, isActive: true },
+    { label: 'Contact', href: base + '/#contact', viewportHighlightId: 'contact' },
+    { label: 'Developer Portal', href: base + '/docs', isButton: true, isActive: $page.url.pathname === '/docs/' },
   ];
 </script>
 
