7a765dd68ab8e17f1c3860d426e150dd05bc66ef
Joe Caputo <joseph.caputo@projectliberty.io>
2024-11-12 17:38:40 -0500

    feat: phone and table rendering for main DevPortal page (#71)
    # Description
    
    Render responsive design for phone & tablet.
    
    - Closes #33 
    
    ## Screenshot(s)
    <img width="378" alt="image"
    src="https://github.com/user-attachments/assets/0535423c-2327-42fb-9d10-a9d4517a7125">
    
    <img width="378" alt="image"
    src="https://github.com/user-attachments/assets/36297975-8722-47a1-9da6-3b66b253f196">
    
    <img width="378" alt="image"
    src="https://github.com/user-attachments/assets/7c371658-3718-4272-a358-304585d4fbeb">
    
    
    - [x] New Feature
    
    # How to Test?
    
    1. Pull and run locally
    2. Go to "Developer Portal" page
    3. See that the design at different sizes responds appropriately
    
    # Checklist:
    
    - [x] I have performed a self-review of my code
    - [x] I have commented my code & PR, particularly in hard-to-understand
    areas
    - [x] I have checked at all the breakpoints to make sure it works on all
    screen sizes

--- src/routes/docs/components/DocsMain.svelte
+++ src/routes/docs/components/DocsMain.svelte
@@ -2,12 +2,12 @@
   import DocsMainImage from './DocsMainImage.svelte';
 </script>
 
-<section class="freq-container flex sm:flex-col lg:min-h-[800px] lg:flex-row">
-  <div class="sm:w-full lg:block lg:w-1/2">
+<section class="freq-container flex flex-col lg:min-h-[800px] lg:flex-row vertical-md:flex-row">
+  <div class="vertical-md::w-1/2 block w-full justify-center lg:w-1/2 lg:justify-start vertical-md:justify-start">
     <DocsMainImage />
   </div>
-  <div class="flex flex-col gap-6 sm:pt-f32 lg:w-1/2 lg:pt-f224">
-    <h2 class=" title-75 text-navy sm:text-h2">Get Started with Frequency Development</h2>
+  <div class="flex flex-col gap-6 pt-f32 lg:w-1/2 lg:pt-f224 vertical-md:pt-f224">
+    <h2 class=" title-75 text-h2 text-navy">Get Started with Frequency Development</h2>
     <p class="body text-h5 font-bold text-black sm:text-normal">
       Frequency offers a variety of open source tools and documentation in the Developers Portal to help you rapidly get
       applications up and running on Frequency.
--- src/routes/docs/components/DocsMainImage.svelte
+++ src/routes/docs/components/DocsMainImage.svelte
@@ -1,4 +1,5 @@
 <!-- Desktop -->
 <div
-  class="relative w-full bg-[url(/src/lib/assets/dev-portal.svg)] bg-cover bg-center bg-no-repeat sm:h-[300px] lg:h-full"
+  class="relative min-h-[300px] w-full bg-[url(/src/lib/assets/dev-portal.svg)] bg-cover bg-center bg-no-repeat md:mt-0 md:min-h-[600px]
+  lg:h-full"
 ></div>
