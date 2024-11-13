4e5051997aefef5b325163ed8f1e5746c00a0835
Wil Wade <wil.wade@projectliberty.io>
2024-11-13 17:21:01 -0500

    Explore Tablet View (#77)
    # Description
    
    Explore design for Tablet came in.
    
    - Closes #75 
    - Don't do clicking unless on the large view
    - Removed unneeded vertical breakpoints
    - Had to switch to grid for easy setup on the small and md views
    - Fixed navy bg full width
    - Fixed some brs to be hidden on smaller views.

--- src/components/HAccordion.svelte
+++ src/components/HAccordion.svelte
@@ -2,6 +2,7 @@
   import { createEventDispatcher, type Snippet } from 'svelte';
   import { Button } from '@frequency-chain/style-guide';
   import Vector from '../lib/assets/Vector.svg';
+  import { MAX_MOBILE_WIDTH } from '$lib/consts';
 
   const dispatch = createEventDispatcher();
 
@@ -17,12 +18,13 @@
   let { isOpen = $bindable(false), sectionNumber, sectionLabel, iconSrc, children, url }: HAccordionProps = $props();
 
   let isEven: boolean = parseInt(sectionNumber, 10) % 2 === 0;
-  let textAlignClasses = `lg:text-left vertical-md:text-left ${isEven ? 'text-right' : ''}`;
-  let borderClasses = `lg:border-l-[1.5px] lg:pl-f12 lg:border-r-0 lg:pr-3 vertical-md:border-l-[1.5px] vertical-md:pl-f12 vertical-md:border-r-0 vertical-md:pr-3 ${isEven ? 'border-r-[1.5px] pr-f12' : 'border-l-[1.5px] pl-f12'}`;
-  let contentAlignClasses = `lg:justify-normal vertical-md:justify-normal ${isEven ? 'justify-end' : ''}`;
+  let textAlignClasses = `md:text-left ${isEven ? 'text-right' : ''}`;
+  let borderClasses = `md:border-l-[1.5px] lg:pl-f12 md:border-r-0 lg:pr-3 ${isEven ? 'border-r-[1.5px] sm:pr-f12' : 'border-l-[1.5px] sm:pl-f12'}`;
+  let contentAlignClasses = `md:justify-normal ${isEven ? 'justify-end' : ''}`;
 
   function toggle() {
-    if (!isOpen) {
+    // Don't toggle unless we are open and in lg view
+    if (!isOpen && (document.body?.scrollWidth || MAX_MOBILE_WIDTH) >= MAX_MOBILE_WIDTH) {
       isOpen = true;
       dispatch('toggled', { index: sectionNumber, isOpen });
     }
@@ -34,7 +36,7 @@
 <div
   id={`explore_${sectionNumber}`}
   onclick={toggle}
-  class="{`lg:w-min-[150px] vertical-md:w-min-[150px] flex cursor-pointer flex-col ${borderClasses} border-grayBorder px-f32 lg:transition-all vertical-md:transition-all ${isOpen ? 'lg:w-f320 vertical-md:w-f320' : 'lg:w-[132px] vertical-md:w-[132px]'}`}}"
+  class={`flex flex-col ${borderClasses} border-grayBorder px-f32 lg:pb-f96 lg:pt-f96 lg:transition-all ${isOpen ? 'lg:w-[316px]' : 'shrink-0 lg:w-[132px] lg:cursor-pointer'}`}
 >
   <div class={`-mt-[9px] text-clip text-nowrap font-title text-h3 font-normal text-white ${textAlignClasses}`}>
     .{parseInt(sectionNumber, 10).toString().padStart(2, '0')}
@@ -42,7 +44,7 @@
   {#if iconSrc}
     <div
       id={`explore_${sectionNumber}_icon`}
-      class={`flex w-full flex-row pt-[6px] lg:justify-normal vertical-md:justify-normal ${contentAlignClasses} `}
+      class={`flex w-full flex-row pt-[6px] lg:justify-normal ${contentAlignClasses} `}
     >
       <img src={iconSrc as string} alt="" class="h-[65px] w-[65px]" />
     </div>
@@ -54,12 +56,10 @@
     {@render sectionLabel()}
   </div>
 
-  <div
-    class={`lg:transition-all vertical-md:transition-all ${isOpen ? 'block' : 'block lg:hidden vertical-md:hidden'}`}
-  >
+  <div class={`lg:transition-all ${isOpen ? 'block' : 'block lg:hidden'}`}>
     <div
       id={`explore_${sectionNumber}_content`}
-      class={`sm max-h-f224 max-w-[515px] overflow-hidden pb-f12 pl-0 pr-f8 pt-4 text-white md:max-w-full lg:max-w-[280px] vertical-md:max-w-[280px] ${textAlignClasses}`}
+      class={`sm overflow-hidden pb-f12 pl-0 pr-f8 pt-4 text-white md:max-w-full ${textAlignClasses}`}
     >
       {@render children()}
     </div>
