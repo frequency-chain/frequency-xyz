87a03ffe8541c1b5d2e63aad6b1ec6fc4624f054
Wil Wade <wil.wade@projectliberty.io>
2024-11-13 17:21:01 -0500

    Explore Tablet View (#77)
    # Description
    
    Explore design for Tablet came in.
    
    - Closes #75 
    - Don't do clicking unless on the large view
    - Removed unneeded vertical breakpoints
    - Had to switch to grid for easy setup on the small and md views
    - Fixed navy bg full width
    - Fixed some brs to be hidden on smaller views.

--- src/routes/docs/components/Explore.svelte
+++ src/routes/docs/components/Explore.svelte
@@ -26,16 +26,13 @@
 
 <div
   id="explore_section"
-  class="freq-container mt-f96 flex w-full flex-col gap-f12 sm:pt-f20 lg:flex-row lg:gap-0 lg:bg-navy lg:px-f128 lg:py-f96 vertical-lg:min-h-[550-px] vertical-lg:w-[100%] vertical-md:flex-row vertical-md:gap-0"
+  class="freq-container mt-f96 flex w-full flex-col gap-f12 pb-f96 pt-f20 lg:flex-row lg:gap-0 lg:px-f128 lg:py-f96"
 >
-  <div
-    id="explore_description"
-    class="relative flex flex-row bg-navy pr-f12 text-teal sm:px-f32 vertical-lg:min-h-[550px]"
-  >
+  <div id="explore_description" class="relative flex w-full flex-row px-f32 text-teal lg:w-1/2 lg:pt-f96">
     <div class="flex flex-col gap-f32">
       <span class="h1 mt-f8 text-clip text-wrap font-title font-normal">Explore &amp; Build</span>
 
-      <div class="text-sm text-white sm:max-w-[515px] sm:pb-[100px] lg:pb-[200px]">
+      <div class="pb-f64 text-sm text-white md:pb-f80">
         Frequency offers simple blockchain integration for your applications&mdash;bridging the gap between your app and
         the decentralized web. Frequency Gateway offers a suite of tools and services that make it easy to connect your
         applications to the Frequency blockchain. This allows your development team to focus on building outstanding
@@ -53,8 +50,8 @@
   </div>
 
   <!-- min width selected based on description section + 1 open accordion + 3 closed accordions -->
-  <div class="w-full bg-navy sm:px-f32 sm:py-f16">
-    <div class="flex flex-col gap-f4 lg:min-w-[716px] lg:flex-row vertical-md:flex-row">
+  <div class="w-full sm:px-f32 sm:py-f16">
+    <div class="grid grid-cols-1 gap-x-f4 gap-y-f48 md:grid-cols-2 lg:flex">
       <HAccordion
         sectionNumber="1"
         iconSrc={ProviderIcon}
@@ -75,7 +72,7 @@
         bind:isOpen={sectionOpenStates[2]}
         url="https://docs.frequency.xyz/Guides/Gateway.html"
       >
-        {#snippet sectionLabel()}Frequency<br />Gateway{/snippet}
+        {#snippet sectionLabel()}Frequency<br class="hidden lg:block" /> Developer<br class="hidden lg:block" /> Gateway{/snippet}
         is a collection of easy-to-deploy microservices that provide simple REST APIs for interacting with Frequency. Configure
         and deploy only the components you need.
       </HAccordion>
@@ -87,7 +84,7 @@
         bind:isOpen={sectionOpenStates[3]}
         url="https://docs.frequency.xyz/Guides/SSO.html"
       >
-        {#snippet sectionLabel()}Add Single<br />Sign-on to<br />your App{/snippet}
+        {#snippet sectionLabel()}Add Single<br class="hidden lg:block" /> Sign-on to<br class="hidden lg:block" /> your App{/snippet}
         SIWF provides comprehensive documentation and tools to integrate with Frequency Access, an easy-to-use single sign-on
         identity solution for users on Frequency. SIWF simplifies the authentication process, allowing users to access the
         entire Frequency ecosystem effortlessly while maintaining control over their personal data.</HAccordion
@@ -100,7 +97,7 @@
         bind:isOpen={sectionOpenStates[4]}
         url="https://docs.frequency.xyz/"
       >
-        {#snippet sectionLabel()}Frequency<br />Core{/snippet}
+        {#snippet sectionLabel()}Frequency<br class="hidden lg:block" /> Core{/snippet}
         Here you will find detailed information about the building blocks of Frequency including technical overviews, advanced
         tutorials, API documentation, code samples and more&mdash;everything you need to build your first Frequency application.</HAccordion
       >
