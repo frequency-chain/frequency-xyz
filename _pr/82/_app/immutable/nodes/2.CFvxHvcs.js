f3f674cff633e66d6a1af4a07167e4563f63ac02
shannonwells <shannonwells@users.noreply.github.com>
2024-11-14 20:23:59 +0000

    Deploying to gh-pages from @ frequency-chain/frequency-xyz@65ca48d12be100f578f144f75a1f5813989ea447 🚀

---
+++ _pr/82/_app/immutable/nodes/2.CFvxHvcs.js
@@ -0,0 +1,1 @@
+import{a as e,t as s}from"../chunks/disclose-version.CWLpVtVb.js";import"../chunks/legacy.DDbevF9d.js";import{c as i,r as m}from"../chunks/runtime.Dca_6ewH.js";import{s as p}from"../chunks/slot.BcRL36Yl.js";var n=s('<div class="freq-container"><!></div>');function v(a,r){var o=n(),t=i(o);p(t,r,"default",{}),m(o),e(a,o)}export{v as component};
