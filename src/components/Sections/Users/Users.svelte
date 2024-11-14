89a5c311dc33d16f653684822fff294c346454a5
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2024-11-14 08:16:48 -0800

    Update src/components/Sections/*
    Co-authored-by: Wil Wade <wil.wade@projectliberty.io>

--- src/components/Sections/Users/Users.svelte
+++ src/components/Sections/Users/Users.svelte
@@ -6,14 +6,14 @@
   import PlaceholderUsers from '$lib/assets/UsersIcons/PlaceholderUsers.png';
 </script>
 
-<SectionWrapper id="users" class="relative bg-navy text-white sm:pb-f128 sm:pt-0 lg:py-f128">
+<SectionWrapper id="users" class="relative bg-navy pb-f128 pt-0 text-white lg:py-f128">
   <SectionContent class="freq-container flex flex-col gap-f56">
-    <div class="flex sm:flex-col-reverse sm:gap-f24 md:gap-0 lg:flex-row lg:gap-f56">
+    <div class="flex flex-col-reverse gap-f24 md:gap-0 lg:flex-row lg:gap-f56">
       <UsersHeader />
       <img
         src={PlaceholderUsers}
         alt="PlaceholderUsers"
-        class="ml-auto max-w-[700px] object-contain sm:w-full lg:-mr-f64 lg:w-[45%]"
+        class="ml-auto w-full max-w-[700px] object-contain lg:-mr-f64 lg:w-[45%]"
       />
     </div>
     <UsersItems />
--- src/components/Sections/Users/UsersItem.svelte
+++ src/components/Sections/Users/UsersItem.svelte
@@ -5,7 +5,7 @@
   export let body = '';
 </script>
 
-<div class="sm:max-w-full md:max-w-[500px] lg:max-w-[370px]">
+<div class="max-w-full md:max-w-[500px] lg:max-w-[370px]">
   <div class="flex items-center gap-f32 pb-f16">
     <img src={icon} alt={iconAlt} />
     <h3 class="title-44">{title}</h3>
