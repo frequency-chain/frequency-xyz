2a569a3097e40e581dbdcba33fa519f9195a2d65
Wil Wade <wil.wade@projectliberty.io>
2024-11-15 15:16:45 -0500

    Top Section and Animation (#86)
    # Description
    
    Completed the top section of the page and the associated animation
    
    - Closes #59 
    - Closes #85 
    - Part of #34 
    
    Note: Some of those section margins end up quite large. We'll be
    cleaning those up at the end so we can make sure the margins are
    consistent.

---
+++ src/components/Sections/Top/Top.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import SectionWrapper from './../SectionWrapper.svelte';
+  import SlideIn from '../../SlideIn.svelte';
+  import HomeAnimation from '../../Animations/Home.svelte';
+  import MsaCounter from './MsaCounter.svelte';
+
+  // top-[calc(255px-((100vw-1300px)*0.075))] !?!?!?!
+  // That's to be able to responsively handle the movement of the animation
+</script>
+
+<SectionWrapper class="freq-container flex flex-col pb-f64 md:flex-row md:items-center md:justify-center">
+  <div class="mb:h-[500px] relative mb-5 flex h-[300px] w-full self-stretch sm:h-[380px] md:mb-0 md:w-1/2 lg:h-[700px]">
+    <HomeAnimation />
+    <div
+      class="math absolute left-[20px] top-[25%] sm:left-[24%] md:left-[23%] md:top-[calc(85px-((100vw-1300px)*0.075))] lg:top-[calc(250px-((100vw-1300px)*0.075))] xl:top-[220px]"
+    >
+      <SlideIn>
+        <MsaCounter />
+      </SlideIn>
+    </div>
+  </div>
+  <div class="w-full sm:max-w-[260px] sm:pl-f32 md:w-1/2 md:max-w-[300px] md:pl-0 lg:max-w-[380px]">
+    <SlideIn>
+      <div class="title-70 w-full text-primary sm:text-left">Bringing</div>
+      <div class="title-70 relative w-full text-primary sm:text-right">Humanity</div>
+      <div class="w-full italic text-teal sm:text-[28px] lg:text-[32px]">to the</div>
+      <div class="title-70 w-full text-primary sm:text-left">People's</div>
+      <div class="title-70 w-full text-primary sm:text-right">Internet</div>
+    </SlideIn>
+  </div>
+</SectionWrapper>
--- src/components/Sections/Users/UsersItems.svelte
+++ src/components/Sections/Users/UsersItems.svelte
@@ -33,7 +33,9 @@
 <div class="mx-auto grid w-fit gap-f56 xs:grid-cols-1 lg:grid-cols-3">
   {#each userItems as { icon, iconAlt, title, body }}
     <UsersItem {icon} {iconAlt} {title}>
-      <div slot="body" bind:innerHTML={body} contenteditable="true" class="flex flex-col gap-f16"></div>
+      <div slot="body" class="flex flex-col gap-f16">
+        {@html body}
+      </div>
     </UsersItem>
   {/each}
 </div>
