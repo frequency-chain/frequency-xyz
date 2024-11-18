170fe885747288cb53ddbe78e771a53fbb89191f
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2024-11-18 15:58:08 -0800

    Hot fix/dev portal title animation (#98)
    # Description
    
    Hot fix to add title transitions to the dev portal and clip the height
    of the accordion content as it loads in in the explore section.
    
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

--- src/routes/docs/components/DocsMain.svelte
+++ src/routes/docs/components/DocsMain.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import DevPortalAnimation from '../../../components/Animations/DevPortal.svelte';
+  import SlideIn from '$components/SlideIn.svelte';
 </script>
 
 <section class="freq-container flex flex-col lg:flex-row lg:items-center lg:justify-center">
@@ -7,10 +8,14 @@
     <DevPortalAnimation />
   </div>
   <div class="flex flex-col gap-6 lg:w-1/2">
-    <h1 class="title-70 text-primary">Get Started with Frequency Development</h1>
-    <p class="title-h5 text-black">
-      Frequency offers a variety of open source tools and documentation in the Developers Portal to help you rapidly get
-      applications up and running on Frequency.
-    </p>
+    <SlideIn>
+      <h1 class="title-70 text-primary">Get Started with Frequency Development</h1>
+    </SlideIn>
+    <SlideIn>
+      <p class="title-h5 text-black">
+        Frequency offers a variety of open source tools and documentation in the Developers Portal to help you rapidly
+        get applications up and running on Frequency.
+      </p>
+    </SlideIn>
   </div>
 </section>
