ee0de28d9022b22cd19f3ab402efe3350d00af3a
Wil Wade <wil.wade@projectliberty.io>
2024-11-18 11:22:07 -0500

    Cleanup "Your" section font sizes

--- src/components/Sections/Mission/MissionStatement.svelte
+++ src/components/Sections/Mission/MissionStatement.svelte
@@ -1,10 +1,10 @@
 <div class="bg-navy py-f24 text-white">
-  <div class="freq-container flex items-center justify-center gap-f24">
-    <h2 class="title-70 sm:text-h1 md:text-[70px]">Your:</h2>
+  <div class="flex items-center justify-center gap-f12 md:gap-f24">
+    <h2 class="title-70 text-[30px] sm:text-[46px] md:text-[70px]">Your:</h2>
     <div class="flex flex-col">
-      <span class="font-thin opacity-40 sm:text-[30px] md:text-[50px]">Followers</span>
-      <span class="font-thin sm:text-[50px] md:text-[70px]">Network</span>
-      <span class="font-thin opacity-40 sm:text-[30px] md:text-[50px]">Content</span>
+      <span class="text-[20px] font-thin opacity-40 sm:text-[30px] md:text-[50px]">Followers</span>
+      <span class="text-[30px] font-thin sm:text-[48px] md:text-[70px]">Network</span>
+      <span class="text-[20px] font-thin opacity-40 sm:text-[30px] md:text-[50px]">Content</span>
     </div>
   </div>
 </div>
