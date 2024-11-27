763efe784a249cc4cdbc2aae66bc7ed7caf75148
Claire Olmstead <olmsteadclaire@gmail.com>
2024-11-27 10:07:50 -0800

    update thank you card

--- src/components/GetNotified/ContactThankYou.svelte
+++ src/components/GetNotified/ContactThankYou.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import ThankYouWaves from '$lib/assets/Contact/ThankYouWave.svg';
-  export let onClick = (e: Event) => {};
+
+  export let onClick: (e: MouseEvent) => void;
 </script>
 
-<!-- svelte-ignore a11y_click_events_have_key_events -->
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-<div class="flex cursor-pointer items-center justify-center" onclick={onClick}>
+<button class="flex cursor-pointer items-center justify-center" onclick={onClick}>
   <img src={ThankYouWaves} alt="Frequency waves with 'Thank you!' caption" />
-</div>
+</button>
