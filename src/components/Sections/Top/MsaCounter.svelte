6b66672ac4d80d74fc200cafa2c74a65859dae20
Claire Olmstead <olmsteadclaire@gmail.com>
2025-09-09 14:23:04 -0700

    update metrics to include AT Protocol Content Feed Users

--- src/components/Sections/Top/MsaCounter.svelte
+++ src/components/Sections/Top/MsaCounter.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
   import { onMount, tick } from 'svelte';
   import { fade } from 'svelte/transition';
-  import { getMsaCount } from '$lib/metrics';
+  import { getMsaCount, getTotUsersCount } from '$lib/metrics';
 
-  let msaCount = $state(1_333_820);
+  let msaCount = $state(2_098_604);
+  let totUsersCount = $state(11_298_183);
   const msaCountAnimationMs = 300;
   let updateIntervalMs = 5000;
   let showMsaCount = $state(false);
-  let displayMsaCount = $derived(msaCount.toLocaleString());
+  let displayMsaCount = $derived((msaCount + totUsersCount).toLocaleString());
   // Make this as long as possible for the variable width font handling
   let widthMsaCount = $derived(displayMsaCount.replaceAll(/[0-9]/g, '8'));
 
   async function updateMsaCount(skipAnimation = false) {
     const newMsaCount = await getMsaCount();
-    if (newMsaCount !== msaCount) {
+    const newTotUsersCount = await getTotUsersCount();
+
+    if (newMsaCount !== msaCount || newTotUsersCount !== totUsersCount) {
       // Toggle the animation classes so it is reapplied
       showMsaCount = skipAnimation ? showMsaCount : false;
       await tick();
       await new Promise((r) => setTimeout(r, msaCountAnimationMs + 100));
       msaCount = newMsaCount;
+      totUsersCount = newTotUsersCount;
       showMsaCount = true;
     }
   }
@@ -35,23 +39,25 @@
       clearTimeout(showStatic);
       clearInterval(intervalId);
     };
+
+    // TODO AT Protocol Content Feed Users + New User Activations
   });
 </script>
 
 <div
-  class="bg-black px-4 py-3 text-[40px] leading-none text-white sm:text-[50px] md:text-[calc(100vw/23)] xl:text-[75px]"
+  class="-mt-[1px] bg-black px-4 py-3 text-[40px] leading-none text-white sm:text-[50px] md:text-[calc(100vw/23)] xl:text-[75px]"
 >
+  <div class="text-right text-[12px] font-semibold uppercase md:text-[0.25em]">New User Activations</div>
+  <div class="font-title invisible font-bold tracking-wide">
+    {widthMsaCount}
+  </div>
   {#if showMsaCount}
     <div
       in:fade={{ duration: msaCountAnimationMs }}
       out:fade={{ duration: msaCountAnimationMs }}
-      class="font-title absolute left-0 w-full px-4 text-right font-bold tracking-wide"
+      class="font-title absolute top-[28px] left-0 w-full px-4 text-right font-bold tracking-wide"
     >
       {displayMsaCount}
     </div>
   {/if}
-  <div class="font-title invisible font-bold tracking-wide">
-    {widthMsaCount}
-  </div>
-  <div class="text-right text-[12px] font-semibold uppercase md:text-[0.25em]">New User Activations</div>
 </div>
